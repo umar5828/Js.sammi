229c61bfeccfbe6d3cbab32ba7941358ed38b153
umar5828 <jorayevu30@gmail.com>
2023-10-04 16:30:00 +0500

    A

---
+++ 22-dars/index.js
@@ -0,0 +1,96 @@
+// // "use script";
+// // let x=10;
+// // let y=x;
+
+// // y=y+5;
+// // console.log(x);
+// // console.log(y);
+
+// // let isMarried=false;
+// // let isStudent=isMarried;
+// // isStudent=true;
+// // console.log(isMarried);
+// // console.log(isStudent);
+
+// // const number ={
+// //     x:10,
+// //     y:5,
+// // }
+// // const newNumber=number //havola
+
+// // newNumber.x=15
+
+// // console.log(number)
+// // console.log(newNumber)
+
+// // function nusxaObj(obj){
+// //     let objNusxa ={}
+
+// //     for(let key in obj){
+// //         objNusxa[key]=obj[key]
+// //     }
+// //     return objNusxa
+// // }
+
+// // const newNumbers = nusxaObj(numbers)
+
+// // newNumbers.x=15
+// // newNumbers.z.a=10
+// // console.log(numbers)
+// // console.log(newNumbers)
+// // const numbers ={
+// //     x:10,
+// //     y:5,
+// // }
+// // const addNumber = {
+// //     z:15,
+// // }
+// // const newNumbers =Object.assign({}, numbers)
+// // const newNumbers =Object.assign(addNumber, numbers)
+
+// // newNumbers.x=15
+
+// // console.log(newNumbers)
+
+
+
+
+// // const arr = [1,2,3]
+// // const newArr = arr.slice()
+
+// // newArr[0]=10    //slice() - massivni nusxalash
+// // console.log(arr)
+// // console.log(newArr)
+
+
+
+// // SPREAD OPERATOR
+// //  const liverpool = ['Salah','Mane']
+// //  const manCity = ['De Bruyne','Sterling']
+// //  const mixPlayer=[...liverpool, ...manCity]
+// //  console.log(mixPlayer);
+
+
+// //serverdan kelgan sonlarni spread orqali qoshish
+// // const call =[1,2,3]
+// // function sum(a,b,c){
+// //     console.log(a+b+c)
+// // }
+// // sum(...call)
+
+
+// const arr=['x','y','z']
+// const newArr=[...arr]
+// newArr[0]='a'
+// console.log(newArr)
+
+
+// const numbers ={
+//     x:10,
+//     y:5,
+// }
+// const newNumbers = {...numbers}
+// newNumbers.x=15
+// console.log(newNumbers)
+// console.log(numbers)
+
