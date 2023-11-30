0790bfc697baf0a4bf82c679eb3a6a3ef7fdccec
umar5828 <jorayevu30@gmail.com>
2023-11-30 12:09:46 +0500

    12.amaliyot uyga vazifasi

--- Vazifa/main.js
+++ Vazifa/main.js
@@ -9,11 +9,23 @@ const SeriesDB = {
   privat: false,
 };
 
-const a = prompt("Oxirgi ko'rganingiz serial nomi");
-const b = prompt("Unga qancha baho bergan bo'lganingizni yozing");
-const c = prompt("Oxirgi ko'rganingiz serial nomi");
-const d = prompt("Unga qancha baho bergan bo'lganingizni yozing");
-SeriesDB.series[a] = b;
-SeriesDB.series[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Oxirgi ko'rganingiz serial nomi");
+  const b = prompt("Unga qancha baho bergan bo'lganingizni yozing");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    SeriesDB.series[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+if (SeriesDB.count < 5) {
+  console.log("Juda kam");
+} else if (SeriesDB.count >= 5 && SeriesDB.count < 10) {
+  console.log("O'rtacha");
+} else if (SeriesDB.count >= 10) {
+  console.log("A'lo");
+}
 
 console.log(SeriesDB);
